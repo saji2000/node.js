7fab21c307c9a44ecffd468a8a40841875c1223a
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-02-04 16:33:46 -0700

    file system async in node.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const {readFile, writeFile} = require('fs');
 
-const first = readFile('./texts/first.txt', 'utf8');
+const first = readFile('./texts/first.txt', 'utf-8',(err, result) => {
+    if(err){
+        console.log(err);
+        return null;
+    }
+    console.log(result);
+});
 
-const second = readFile('./texts/second.txt', 'utf8');
+// const second = readFile('./texts/second.txt', (err, result) => {
+//     if(err){
+//         console.log(err);
+//         return null;
+//     }
+//     console.log(result);
+// });
+// console.log(first, second);
 
-console.log(first, second);
-
-writeFileSync('./texts/result.txt', `Here is the result: ${first}, ${second} \n`, {flag: 'a'});
+// writeFileSync('./texts/result.txt', `Here is the result: ${first}, ${second} \n`, {flag: 'a'});
