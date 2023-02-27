09f4a8f59f2bc957269597e8a58225eb7d9adcac
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-02-27 15:37:24 -0700

    created a html header

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World!</h1>');
 });
 
 server.listen(3000, '127.0.0.1', () => {
