12ac685403aa0eb6626bdb29abca654ebfdb00ba
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-02-27 15:35:38 -0700

    created a server

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-const {readFile, writeFile, write} = require('fs');
+const http = require('http');
 
-readFile('./texts/first.txt', 'utf-8',(err, result) => {
-    if(err){
-        console.log(err);
-        return null;
-    }
-    const first = result;
-    readFile('./texts/second.txt', 'utf-8', (err, result) => {
-        if(err){
-            console.log(err);
-            return null;
-        }
-        const second = result;
-
-        writeFile('./texts/result.txt', `Here is the result: ${first}, ${second} \n`,
-        (err, result) => {
-            if(err){
-                console.log(err);
-                return;
-            }
-            console.log(result);
-        });
-    })
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!');
 });
 
-// const second = readFile('./texts/second.txt', (err, result) => {
-//     if(err){
-//         console.log(err);
-//         return null;
-//     }
-//     console.log(result);
-// });
-// console.log(first, second);
-
-// writeFileSync('./texts/result.txt', `Here is the result: ${first}, ${second} \n`, {flag: 'a'});
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server running');
+});
