fb080c9f7a78f57d1ab204ae46f5159506f7166f
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-02-28 19:21:54 -0700

    adding packages from npm

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const http = require('http');
+const {v4: uuidv4} = require('uuid');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World!</h1>');
-});
+console.log(uuidv4());
 
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Server running');
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1>Hello World!</h1>');
+// });
+
+// server.listen(3000, '127.0.0.1', () => {
+//     console.log('Server running');
+// });
