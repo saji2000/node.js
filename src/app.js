61be7d1e7d7a9439dfe661433258642ce2a36997
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-02-28 19:29:59 -0700

    getting started with post and get methods

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.post('/', (req, res) => {
+    res.send("Post request!");
+});
+
 app.listen(PORT, () => {
     console.log('listening on port');
 });
