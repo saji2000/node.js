487595280710bf8ff0dfbcf958e7b85919805c07
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-03-10 23:03:25 -0700

    GET API, thank God

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
-
 const dotenv = require('dotenv');
-
 const Customer = require('./models/customer');
-
 const app = express();
 
 mongoose.set("strictQuery", false);
