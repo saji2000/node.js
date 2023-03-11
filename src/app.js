c6d3b12e5bbe259651edda42fe88a027b08c8264
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2023-03-11 12:22:37 -0700

    Working with Cors, thank God

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const Customer = require('./models/customer');
 const app = express();
 
 mongoose.set("strictQuery", false);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
