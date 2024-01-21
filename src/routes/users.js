22eb12b373e2ba89a068851d3f27a3bd109d909a
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-20 20:14:09 -0700

    Added a delete request,thank God

--- src/routes/users.js
+++ src/routes/users.js
@@ -77,8 +77,10 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+  const email = req.params.email;
+  users = users.filter((user) => user.email != email);
+
+  res.send(`User with the email  ${email} deleted.`);
 });
 
 module.exports = router;
